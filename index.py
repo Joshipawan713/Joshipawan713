0581b3c4f417cb1f73508793e65bb2c98796ca6f
Pawan Joshi <149384045+Joshipawan713@users.noreply.github.com>
2024-03-27 16:27:14 +0530

    Create index.py
    python file

---
+++ index.py
@@ -0,0 +1,54 @@
+# print('hello world')
+
+# fruits = {"apple", "banana", "cherry"}
+
+# for x in fruits:
+#     print(x)
+    
+# for x in range(0,11):
+#     print(x*2)
+
+# i=2
+# while i<21:
+#     print(i)
+#     i+=2
+
+# cars = ["Ford", "Volvo", "BMW", "Ford", "Volvo", "BMW"]
+
+# y = len(cars)
+# print(y)
+# for x in cars:
+#     print(x)
+
+# def fun():
+#     x="hello python"
+#     print(x)
+    
+# fun()
+
+# a = 330
+# b = 200
+
+# if b > a:
+#     print("b is greater than a")
+# elif b<a:
+#     print("a is greater than b")
+# else:
+#     print("both not greter")
+
+
+#this is single line comment
+'''
+multiple line comment
+'''
+"""
+multiple line comment
+"""
+
+# escape squence character
+# \n - new line 
+# print("hey i am good boy \n and this viewer is also good boy/girl")
+# \" double quote \" - ""
+# print("hey i am \"good boy\" and this viewer is also good boy/girl")
+
+# varibles and data types
